bd55ac2bbdef35b7c425009ce8bee83f864f9ee9
Jorge Ochipinti <jorgeochipinti@Air-de-Jorge.fibertel.com.ar>
2022-05-22 08:51:21 -0300

    meta

--- components/header/Header.js
+++ components/header/Header.js
@@ -4,23 +4,26 @@ import Contact from '../contact/Contact';
 import Link from 'next/link';
 
 const Header = () => {
+    const origin = (typeof window === 'undefined') ? '' : window.location.origin
+
     return (
         <>
             <Head>
                 <title>Creative Coding</title>
                 <meta name="description" content="Creative Codign" />
+                <meta property="og:image" content={`${origin}/creative.png`} />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
             <header className="min-w-full  py-14 px-4 md:px-0 ">
                 <div className="flex items-center container mx-auto max-w-5xl justify-between text-white">
-            <Link href='/'>
-                    <span className="font-coiny text-xl md:text-3xl font-bold cursor-pointer">
-                        <span className="bg-gradient-to-br text-gray-500 from-brand-blue to-brand-purple pr-2 bg-clip-text ">
-                            Creative
+                    <Link href='/'>
+                        <span className="font-coiny text-xl md:text-3xl font-bold cursor-pointer">
+                            <span className="bg-gradient-to-br text-gray-500 from-brand-blue to-brand-purple pr-2 bg-clip-text ">
+                                Creative
+                            </span>
+                            Coding
                         </span>
-                        Coding
-                    </span>
-            </Link>
+                    </Link>
                     <nav aria-label="Contact Menu">
                         <Contact />
                     </nav>
